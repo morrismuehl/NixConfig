29c8a06b0766a4b48ccf2dc6b8945459673095a9
morrismuehl <morrismuehl@gmail.com>
2024-10-14 10:29:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NIXCONFIG
--- lighttheme.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i -r 's/(include)\ .*conf/\1 light-theme.conf/g' /home/morris/.config/kitty/kitty.conf 
-sed -i -r 's/(^vim\.cmd\(\"colorscheme)\ .*\"\)/\1 adwaita\"\)/g' /home/morris/.config/nvim/init.lua 
